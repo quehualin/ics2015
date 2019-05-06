a2f5c1cdf18d9d908b4f4a1ef26d41e7a0ee5f76
v <316259255@qq.com>
2019-05-06 17:27:10 +0800

    nothings

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -37,6 +37,7 @@ static int cmd_q(char *args) {
 }
 
 static int cmd_help(char *args);
+static int cmd_si(char *args);
 
 static struct {
 	char *name;
@@ -46,6 +47,7 @@ static struct {
 	{ "help", "Display informations about all supported commands", cmd_help },
 	{ "c", "Continue the execution of the program", cmd_c },
 	{ "q", "Exit NEMU", cmd_q },
+	{ "si", "Exit NEMU", cmd_si },
 
 	/* TODO: Add more commands */
 
